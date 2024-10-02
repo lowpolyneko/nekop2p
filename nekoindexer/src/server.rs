6fa503c4d6ce0ea1de4f4681c1a6fcbfeb857439
Neko <lowpolyneko@protonmail.ch>
2024-10-02 13:14:44 -0500

    add base peer

--- nekoindexer/src/server.rs
+++ nekoindexer/src/server.rs
@@ -17,14 +17,16 @@ impl IndexerServer {
 
 impl Indexer for IndexerServer {
     async fn register(mut self, c: Context, filename: String) {
+        println!("Registered {filename}");
         let list = self.index.entry(filename).or_default();
-
         list.insert(c.trace_context.span_id.to_string());
     }
     async fn search(mut self, _: Context, filename: String) -> Vec<String> {
+        println!("Queried {filename}");
         self.index.entry(filename).or_default().iter().cloned().collect()
     }
     async fn deregister(mut self, c: Context, filename: String) {
+        println!("Deregistered {filename}");
         let list = self.index.entry(filename).or_default();
 
         list.remove(&c.trace_context.span_id.to_string());
--- nekopeer/src/main.rs
+++ nekopeer/src/main.rs
@@ -1,3 +1,18 @@
-fn main() {
-    println!("Hello, world!");
+use anyhow::Result;
+use tarpc::{
+    client, context,
+    serde_transport::tcp,
+    tokio_serde::formats::Bincode,
+};
+
+use nekop2p::IndexerClient;
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    let transport = tcp::connect("localhost:5000", Bincode::default);
+
+    let client = IndexerClient::new(client::Config::default(), transport.await?).spawn();
+    client.register(context::current(), "test".to_string()).await?;
+
+    Ok(())
 }
