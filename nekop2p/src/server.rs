4e46406051ba7a1959ce27c66213c47de0beb69f
Neko <lowpolyneko@protonmail.ch>
2024-10-27 18:16:43 -0500

    ttl backtrace

--- nekop2p/src/server.rs
+++ nekop2p/src/server.rs
@@ -9,7 +9,7 @@ use crate::Indexer;
 #[derive(Clone)]
 pub struct IndexerServer {
     /// Address of the remote peer
-    addr: SocketAddr,
+    pub addr: SocketAddr,
 
     /// Index shared between all connections
     index: Arc<DashMap<String, DashSet<SocketAddr>>>,
--- nekop2p/src/superpeer/server.rs
+++ nekop2p/src/superpeer/server.rs
@@ -1,6 +1,7 @@
-use std::ops::Deref;
+use std::{ops::Deref, sync::Arc};
 use std::net::SocketAddr;
 
+use dashmap::DashMap;
 use tarpc::context::Context;
 use uuid::Uuid;
 
@@ -8,7 +9,18 @@ use crate::{IndexerServer, SuperPeer};
 
 struct TTLEntry<T> {
     val: T,
-    ttl: i64,
+    ttl: u64,
+}
+
+impl<T> TTLEntry<T> {
+    /// Timed unwrapped
+    fn timed_unwrap(&self) -> Option<&T> {
+        if unix_time() > self.ttl {
+            Some(&self.val)
+        } else {
+            None
+        }
+    }
 }
 
 /// Reference [crate::Indexer] implementation with [SuperPeer] support
@@ -16,15 +28,29 @@ struct TTLEntry<T> {
 pub struct SuperIndexerServer {
     /// Underlying [IndexerServer]
     s: IndexerServer,
+
+    /// Index for back-propogation of queries
+    backtrace: Arc<DashMap<Uuid, TTLEntry<SocketAddr>>>,
+}
+
+impl SuperIndexerServer {
+    fn prune_backtrace_table(self) {
+    }
 }
 
 impl SuperPeer for SuperIndexerServer {
     async fn query(self, _: Context, msg_id: Uuid, ttl: u8, filename: String) {
-
+        // on query, append to backtrace table
+        // TODO don't hardcode TTL
+        self.backtrace.insert(msg_id, TTLEntry { val: self.addr, ttl: unix_time() + 30 });
     }
 
     async fn query_hit(self, _: Context, msg_id: Uuid, ttl: u8, filename: String, peer: SocketAddr) {
-
+        // TODO handle non-existant msg id
+        let back_addr = match self.backtrace.get(&msg_id).unwrap().timed_unwrap() {
+            Some(x) => x,
+            None => return,
+        };
     }
 }
 
@@ -33,4 +59,11 @@ impl Deref for SuperIndexerServer {
     fn deref(&self) -> &Self::Target {
         &self.s
     }
+}
+
+fn unix_time() -> u64 {
+    std::time::SystemTime::now()
+        .duration_since(std::time::UNIX_EPOCH)
+        .unwrap()
+        .as_secs()
 }
