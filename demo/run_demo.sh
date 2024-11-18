d7813cbfc1ebfcb3446aca9cec8550af77c39257
Neko <lowpolyneko@protonmail.ch>
2024-11-17 23:22:39 -0600

    ignore metadata files

--- demo/run_demo.sh
+++ demo/run_demo.sh
@@ -2,7 +2,7 @@
 cargo build --release
 
 # remove downloads if they already exist
-rm -f peer_a/{5..10}k.bin peer_b/{1..4}k.bin peer_b/{8..10}k.bin peer_c/{1..7}k.bin
+rm -f peer_a/{5..10}k.bin peer_b/{1..4}k.bin peer_b/{8..10}k.bin peer_c/{1..7}k.bin **/*.meta
 
 tmux new-session -d -s nekop2p './indexer.sh'
 tmux split-window -h
